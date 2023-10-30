1172bd149027fd3c076b413a9f76fe182df2aebf
Guilherme Neves <guilherme.neves20@hotmail.com>
2023-10-29 22:24:46 -0300

    Inicio

--- TREINAMENTO/f026_2_app.py
+++ TREINAMENTO/f026_2_app.py
@@ -18,3 +18,20 @@ cartas_de_um_baralho = ['carta1', 'carta2', 'carta3', 'carta4']
 # Embaralhar uma lista
 print(random.shuffle(cartas_de_um_baralho))
 print(cartas_de_um_baralho)
+
+# DESAFIO 1
+## Simular a opção de jogar uma moeda e resultar em cara ou coroa, Jogue as opções dentro de uma lista e depois imprima o resultado na tela.abs
+
+# DESAFIO 2
+## Fazer um sorteio entre 5 nomes e imprimir o nome sorteado na tela.
+
+# DESAFIO 3
+## Escolher aleatoriamente um número entre 10 e 100 e imprimir o valor na tela. 
+
+moeda = ['Cara', 'Coroa']
+print(random.choice(moeda))
+
+nomes = ['Guilherme', 'João', 'Pedro', 'Paulo', 'Maria']
+print(random.choice(nomes))
+
+print(random.randint(10, 100))
