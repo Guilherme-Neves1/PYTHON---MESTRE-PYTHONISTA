5627f9a91f466b9c581995a1a04e92a26b1af70b
Guilherme Neves <guilherme.neves20@hotmail.com>
2023-11-03 20:12:04 -0300

    att

---
+++ f043_3_app.py
@@ -0,0 +1,15 @@
+# Iterável
+## Iterável é um tipo de dado que nos permite iterar, ou seja, passar por cada valor dentro dele. Esse dado pode gerar valores ou já possuir valores dentro dele.
+
+for item in range(10):
+  print(item)
+
+print('------------------------------------')
+
+for letra in 'programação':
+  print(letra)
+
+print('------------------------------------')
+
+for item in [2, 3, 4, 5]:
+  print(item)
