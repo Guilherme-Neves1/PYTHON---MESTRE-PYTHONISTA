ea628a01d1c73ed45d1244473d6d7764f9bc192f
Guilherme Neves <guilherme.neves20@hotmail.com>
2023-11-03 20:15:01 -0300

    att

--- f043_3_app.py
+++ f043_3_app.py
@@ -1,7 +1,7 @@
 # Iterável
 ## Iterável é um tipo de dado que nos permite iterar, ou seja, passar por cada valor dentro dele. Esse dado pode gerar valores ou já possuir valores dentro dele.
 
-for item in range(10):
+for item in range(10): # Estamos iterando (passando por cada um dos 10 itens) usando o iterável (range(10))
   print(item)
 
 print('------------------------------------')
