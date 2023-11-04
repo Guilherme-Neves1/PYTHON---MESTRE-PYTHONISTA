32c4e3e228fab86b66bf67187075171601424356
Guilherme Neves <guilherme.neves20@hotmail.com>
2023-11-03 21:48:57 -0300

    att

--- TREINAMENTO/f044_3_app.py
+++ TREINAMENTO/f044_3_app.py
@@ -1,6 +1,7 @@
 # Loop while(laço while)
 
 ## Tentativas de colocar sua senha
+'''
 tentativas = 0
 while tentativas < 3:
   print('Tente novamente.')
@@ -30,4 +31,28 @@ contador = 100
 while contador >= 0:
   print(contador)
   contador -= 1
-  
+'''
+
+# DESAFIO 1
+## Crie um loop while que irá contar e imprimir no console de 1 até 120.
+
+num = 0
+while num <= 120:
+  print(f'{num}')
+  num += 1
+
+# DESAFIO 2
+## Crie um loop while que irá continuamente pedir ao usuário a senha para entrada, e só irá permitir o programa continuar caso ele digite a senha 'secreto'
+
+senha = ''
+while senha != 'secreto':
+  senha = input('Digite a senha: \n')
+print('Bem vindo!')
+
+# DESAFIO 3
+## Crie um loop que conte e imprima na tela o valor em ordem descrescente de 100 para 1.
+
+valor = 100
+while valor >= 0:
+  print(valor)
+  valor -= 1
