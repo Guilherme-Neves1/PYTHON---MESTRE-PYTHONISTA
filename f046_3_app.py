b7664244cede0abf726f1a5b379b6b91339a3971
Guilherme Neves <guilherme.neves20@hotmail.com>
2023-11-03 22:02:13 -0300

    att

--- f046_3_app.py
+++ f046_3_app.py
@@ -1,4 +1,5 @@
 # Continue => ignorar/pular
+'''
 for numero in range(100):
   if numero % 2 ==0:
     print(numero)
@@ -18,3 +19,21 @@ for fruta in frutas:
   if fruta == 'manga':
     continue
   print(f'{fruta} adicionada a dieta.')
+'''
+
+# DESAFIO 1
+## Ao chegar ao estilo 'Rap', o mesmo não deve ser impresso na tela.
+estilos = ['Hip-Hop', 'Rock', 'Rap', 'Pop']
+for estilo in estilos:
+  if estilo == 'Rap':
+    continue
+  print(estilo)
+
+# DESAFIO 2
+## Use break ou continue para que a seguinte condição aconteça:
+## Ao chegar em 'Rock', a execução deve parar.abs
+estilos = ['Hip-Hop', 'Rock', 'Rap', 'Pop']
+for estilo in estilos:
+  if estilo == 'Rock':
+    break
+  print(estilo)
