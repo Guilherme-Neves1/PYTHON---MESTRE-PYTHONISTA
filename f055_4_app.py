296a484a523430644eb43ee122807d986ef7374b
Guilherme-Neves1 <guilherme.neves20@hotmail.com>
2023-11-12 16:01:16 -0300

    update

--- f055_4_app.py
+++ f055_4_app.py
@@ -1,4 +1,5 @@
 # **kwargs(Keyword argumentos) => Argumentos nomeados
+'''
 def concatenar(**palavras):
   frase = ''
   for palavra in palavras.values():
@@ -6,3 +7,16 @@ def concatenar(**palavras):
   print(frase)
 
 concatenar(a='Nós', b='somos', c='Pythonistas', d='profissionais')
+'''
+
+# Exemplo
+def fazer_calculos(nome, *args, **kwargs):
+  print(nome)
+  print(args)
+  print(kwargs)
+  for arg in args:
+    print(arg)
+  for kwarg in kwargs.values():
+    print(kwarg)
+
+fazer_calculos("Franklyn", 1, 2, 3, 4, a=5, b=6, c=7, d=8)
