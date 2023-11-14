78b15a7cbb49f31d470350449fa655f0e97179ef
Guilherme-Neves1 <guilherme.neves20@hotmail.com>
2023-11-13 22:15:14 -0300

    update

--- f056_4_app.py
+++ f056_4_app.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 
+'''
 def depositar_dinheiro():
     print("Depositando dinheiro")
 
@@ -27,3 +28,37 @@ resultado = pai(1)
 resultado()
 
 # Decorators
+def meu_decorator(funcao):
+    def wrapper():
+        print("Antes")
+        funcao()
+        print("Depois")
+    return wrapper
+
+@meu_decorator
+def parabenizar():
+    print("Parabéns!!!!")
+
+parabenizar()
+
+# resultado = meu_decorator(parabenizar)
+# resultado() # O @ substitui essas linhas
+'''
+
+# DESAFIO 1
+## Crie um decorador que irá pegar a função que for passado para ele e imprimir o horário atual antes de executar a função e depois imprimir o horário após ter finalizado a execução da função.
+## Dica: Usar o módulo datetime para conseguir o horário atual.
+
+def monitorar_horario(funcao):
+  def monitor():
+    print(datetime.now())
+    funcao()
+    print(datetime.now())
+  return monitor
+
+@monitorar_horario
+def hora_boa():
+  print("Hora boa!!!")
+
+hora_boa()
+  
