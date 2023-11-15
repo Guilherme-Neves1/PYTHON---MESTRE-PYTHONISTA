ec9fa7d9bc9108678df54738108805ba4da1ab05
Guilherme-Neves1 <guilherme.neves20@hotmail.com>
2023-11-15 14:16:55 -0300

    update

--- f070_5_app.py
+++ f070_5_app.py
@@ -7,3 +7,10 @@ for i in range(5):
 print(numeros)
 
 # Map
+nomes = ['Larissa', 'Rafael', 'Marcus', 'John']
+def aprovar_pessoa(nome):
+  # Pode-se fazer uma lógica mais complexa
+  # Ex.: Verificar a pontuação do aluno em um banco de dados; Verificar se o pagamento foi feito em uma planilha etc.
+  return nome + ' APROVADO'
+
+print(list(map(aprovar_pessoa, nomes)))
