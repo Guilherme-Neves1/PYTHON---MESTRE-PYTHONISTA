b9d6f8bc1cf50fda9d3747bd0353ffc843e6d21d
Guilherme-Neves1 <guilherme.neves20@hotmail.com>
2023-11-15 07:40:17 -0300

    update

--- f064_5_app.py
+++ f064_5_app.py
@@ -12,5 +12,17 @@ pessoa = ['Carol', 18, 1.60, 'Mike', 50, 1.85]
 
 # Solução: criar um Dicionário
 dicionario_pessoa = {'nome': 'Carol', 'idade': 18, 'altura': 1.60}
-print(dicionario_pessoa)
+# pessoa_2 = dict(nome='Carol', idade=18, altura=1.60)
 
+print(dicionario_pessoa)
+# print(pessoa_2)
+# print(pessoa_2['idade'])
+
+print(dicionario_pessoa.keys())
+print(dicionario_pessoa.values())
+print(dicionario_pessoa.items())
+
+# Iterar sobre um dicionário
+for item in dicionario_pessoa.items():
+  print(item)
+  print(item[1])
