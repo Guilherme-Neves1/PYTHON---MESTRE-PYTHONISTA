cb9e1a0ff60b0e72ec0a11f545c3600c2c9493d2
Guilherme-Neves1 <guilherme.neves20@hotmail.com>
2023-11-15 11:39:55 -0300

    update

--- f065_5_app.py
+++ f065_5_app.py
@@ -1,1 +1,23 @@
 # Tuplas
+## São uma maneira de armazenar valores que devem ser alterados.
+
+# PARA NÃO PRECISAR FAZER:
+'''
+site1 = 'youtube.com'
+site2 = 'google.com'
+site3 = 'facebook.com'
+
+ou
+
+valor1 = 25
+valor2 = 33
+valor3 = 54
+'''
+
+# PODEMOS USAR AS TUPLAS
+sites = ('youtube.com', 'google.com', 'facebook.com')
+valores = (25, 33, 54)
+
+print(sites[2])
+print(valores[0])
+
