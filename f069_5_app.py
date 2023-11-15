af6b55664f8d7bba7bc4b6d854da3389892dba7c
Guilherme-Neves1 <guilherme.neves20@hotmail.com>
2023-11-15 13:56:03 -0300

    update

--- f069_5_app.py
+++ f069_5_app.py
@@ -42,6 +42,7 @@ print(pessoas)
 
 '''
 
+'''
 produtos = [
   ('Celular', 750),
   ('Bicicleta', 1500),
@@ -55,3 +56,51 @@ print(produtos)
 # Índice 0 = nome, então será ordenado em ordem crescente por nome
 produtos.sort(key=itemgetter(0)) 
 print(produtos)
+
+# ORDEM DECRESCENTE
+produtos.sort(key=itemgetter(0), reverse=True) 
+print(produtos)
+
+matriz = [[5, 10], [15, 21], [1, 5]]
+matriz.sort(key=itemgetter(1))
+print(matriz)
+'''
+
+# DESAFIO 1 
+## Ordene a lista de produtos abaixo pelo preço em ordem crescente
+produtos = [
+  {'nome': 'Celular',
+   'preco': 1500
+   },
+  {'nome': 'Monitor',
+   'preco': 600
+   },
+  {'nome': 'Microfone',
+   'preco': 300
+   },
+]
+
+produtos.sort(key=itemgetter('preco'))
+print(produtos)
+
+print(' ')
+print(' ')
+# DESAFIO 2
+## Ordene em ordem decrescente a lista de equipamento_filmagem por valor
+equipamento_filmagem = [
+  ('Tripé', 300),
+  ('Câmera', 1700),
+  ('Iluminação', 200)
+]
+
+equipamento_filmagem.sort(key=itemgetter(1), reverse=True)
+print(equipamento_filmagem)
+
+print(' ')
+print(' ')
+# DESAFIO 3
+## Ordene em ordem crescente a cotacao_moedas com base no valor da moeda
+cotacao_moedas = [['usd', 5.25], ['brl', 1.56], ['eur', 6.47]]
+
+cotacao_moedas.sort(key=itemgetter(1))
+print(cotacao_moedas)
