2351f21f61ca32efaa4c1bd9ebee0eaa2731e936
Guilherme-Neves1 <guilherme.neves20@hotmail.com>
2023-11-15 17:11:36 -0300

    update

---
+++ TREINAMENTO/modulo_5/f074_5_app.py
@@ -0,0 +1,18 @@
+# Conversão entre tipos
+'''
+idade = int(input('Digite a sua idade: '))
+print(idade > 17)
+
+ano_publicacao = 2020
+print("Este livro foi criado em " + str(ano_publicacao))
+
+altura = float(input("Qual a altura da parede?"))
+print(altura > 2.50)
+'''
+
+# Conversões entre coleções(listas, tuplas, sets)
+saudacao = 'Hello!'
+
+print(list(saudacao))
+print(set(saudacao))
+print(tuple(saudacao))
