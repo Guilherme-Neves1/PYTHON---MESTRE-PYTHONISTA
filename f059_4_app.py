75584fcd66ba9cb1b373d328bbeb3e37d7afee5d
Guilherme-Neves1 <guilherme.neves20@hotmail.com>
2023-11-14 22:04:59 -0300

    update

---
+++ f059_4_app.py
@@ -0,0 +1,10 @@
+# Ao invés de:
+preco_1 = 10
+preco_2 = 20
+preco_3 = 30
+
+# Posso fazer Listas:
+precos = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+print(precos[0]) # Acessar por índice
+
+print(precos[precos.index(100)]) # Enconter o index
