ee4da000ded7103f53afca988c806a3e445fac89
Guilherme-Neves1 <guilherme.neves20@hotmail.com>
2023-11-15 23:07:49 -0300

    update

---
+++ f080_5_app.py
@@ -0,0 +1,14 @@
+# Logging => Manter o histórico do que está acontecendo e do que já aconteceu.
+
+import logging
+'''
+Níveis de logging:
+
+debug
+info
+warning
+error
+critical
+'''
+
+logging.critical("Logging nível critical")
