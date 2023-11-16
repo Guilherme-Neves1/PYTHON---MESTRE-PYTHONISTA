4e5746c4c840ad72ea5ea59a31d18d33127144e0
Guilherme-Neves1 <guilherme.neves20@hotmail.com>
2023-11-15 23:19:38 -0300

    update

---
+++ modulo_5_logging/f080_5_app.py
@@ -0,0 +1,23 @@
+# Logging => Manter o histórico do que está acontecendo e do que já aconteceu.
+
+import logging
+'''
+Níveis de logging:
+
+debug    - Só estou passando informações para devs.
+info     - Só estou informando algo que está acontecendo no programa, mas que não é um erro.
+warning  - Só estou alertando sobre algo que está acontecendo, possívelmente fora do esperado, que ainda não é um erro mas pode gerar um futuramente.
+error    - Um erro que ocorreu na aplicação
+critical - Um erro com consequências graves que acaba de ocorrer na aplicação
+'''
+
+# Logging só exibe mensagens do nível warning pra cima por padrão.
+# Para burlar isso usamos basicConfig
+
+logging.basicConfig(level=logging.DEBUG, filename='app.log', filemode='a', format='%(levelname)s - %(message)s') # Setar o nível
+logging.debug("Logging nível debug")
+logging.info("Logging nível info")
+
+logging.warning("Logging nível warning")
+logging.error("Logging nível error")
+logging.critical("Logging nível critical")
